a5bae8e29b3f0006e75cc5ad3e2634241c4fb2b6
Will Currie <will@currie.id.au>
2015-12-30 10:40:31 +1100

    Add DetailToTsv

--- src/main/scala/io/github/binaryfoo/gclog/SizeDelta.scala
+++ src/main/scala/io/github/binaryfoo/gclog/SizeDelta.scala
@@ -18,6 +18,7 @@ object SuffixExpander {
   def toBytes(v: String): Long = {
     val multiplier = v.charAt(v.length - 1) match {
       case 'K' => 1024
+      case '%' => 1 // hack
     }
     v.substring(0, v.length - 1).toLong * multiplier
   }
